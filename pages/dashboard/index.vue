7187e92239fc7e522b1aaf44f05b47d0b8078097
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-04-16 08:41:04 +0800

    cleaned up the dashboard

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -4,39 +4,6 @@
       <v-col lg="10" md="10" sm="12" cols="12">
 
         <v-row class="mb-10">
-          <v-col>
-            <v-card>
-              <v-card-text>
-                <v-btn color="#448AFF" @click="count++">
-                  Click Me
-                </v-btn>
-                <div>You have clicked it {{ count }} times.</div>
-
-                <v-btn color="primary" @click="$nuxt.$loading.start()">
-                  Start Loading
-                </v-btn>
-
-                <v-btn color="#42A5F5" @click="$nuxt.$loading.finish()">
-                  End Loading
-                </v-btn>
-
-                <v-btn @click="fetchData()">
-                  Fetch
-                </v-btn>
-
-                <v-btn>
-                  Fetch Uesr
-                </v-btn>
-
-                <div>
-                  {{ $auth.loggedIn }}  
-                </div>
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
-
-        <v-row class="mb-10">
           <v-col lg="6" md="6" sm="12" cols="12">
             <v-card height="100%">
               <v-card-text>
