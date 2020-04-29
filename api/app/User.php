2f6554622158538c9ae785d796dc4b3b0d6eaf41
Carlo Miguel Dy <carlomigueldy@gmail.com>
2020-04-30 04:36:45 +0800

    api(UserModel): removed roles from $with attrs

--- api/app/User.php
+++ api/app/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable
      * @var Array
      */
     protected $with = [
-        'roles'
+        // 
     ];
 
     /**
@@ -111,5 +111,17 @@ class User extends Authenticatable
         }
         
         return $this->assignRole($role);
+    }
+
+    /**
+     * Gets the first role from array.
+     * 
+     * @return String
+     */
+    public function getFirstRole()
+    {
+        $roles = $this->getRoleNames()->toArray();
+
+        return array_shift($roles);
     }
 }
